e99b39e5be43e26a97c90138757d5284957ad445
Erik Doernenburg <erik@doernenburg.com>
2020-10-25 17:43:12 +0100

    Simplified markup and component structure.

--- vue/quvyn.js
+++ vue/quvyn.js
@@ -24,8 +24,14 @@ Vue.component('qv-comment-section', {
                 body: JSON.stringify({text: markdown})
             })
                 .then(response => response.text())
-                .then(text => this.preview = text)
+                .then(text => this.preview = text ? text : '')
         }
+    },
+    created() {
+        let p = encodeURIComponent(window.location.pathname)
+        fetch(this.baseurl + "/comments?p=" + p)
+            .then(response => response.json())
+            .then(json => this.comments = json.comments)
     },
     data() {
         return {
@@ -33,24 +39,18 @@ Vue.component('qv-comment-section', {
             preview: ''
         }
     },
-    created() {
-        fetch(this.baseurl + "/comments?p=%2f")
-            .then(response => response.json())
-            .then(json => this.comments = json.comments)
-    },
     template: `
         <section class="qv-comment-section">
-            <h2>Comments</h2>
-            <qv-summary :comments="comments"></qv-summary>
+            <qv-heading :comments="comments"></qv-heading>
+            <p v-if="this.comments.length === 0">No comments yet</p>
             <qv-list :comments="comments"></qv-list>
-            <h3>Leave your comment</h3>
             <qv-comment-editor :preview="preview" @post-comment="postComment" @get-preview="getPreview"></qv-comment-editor>
         </section>
     `
 })
 
 
-Vue.component('qv-summary', {
+Vue.component('qv-heading', {
     props: {
         comments: {
             type: Array,
@@ -63,13 +63,11 @@ Vue.component('qv-summary', {
         }
     },
     template: `
-        <div class="qv-summary">
-            <p>
-                <span v-if="this.count === 0">No comments yet</span>
-                <span v-else-if="this.count === 1">1 comment</span>
-                <span v-else>{{ this.count }} comments</span>
-            </p>
-        </div>
+        <h2 class="qv-heading">
+            <span v-if="this.count === 0">Comments</span>
+            <span v-else-if="this.count === 1">Comments</span>
+            <span v-else>{{ this.count }} Comments</span>
+        </h2>
     `
 })
 
@@ -81,14 +79,31 @@ Vue.component('qv-list', {
             required: true
         }
     },
+
+    filters: {
+        formatTimestamp: function (text) {
+            return luxon.DateTime.fromISO(text).toLocaleString(luxon.DateTime.DATETIME_MED)
+        }
+    },
     template: `
-        <div class="qv-list">
-            <ul>
-                <li v-for="c in comments">
-                    <qv-comment :comment="c"></qv-comment>
-                </li>
-            </ul>
-        </div>
+        <ul class="qv-list">
+            <li v-for="comment in comments" class="qv-comment">
+                <div class="qv-metadata">
+                    <div class="qv-avatar">
+                        <img :src="comment.authorGravatar + '?r=pg&s=40'">
+                    </div>
+                    <div class="qv-author">
+                        <span v-if="comment.authorName">{{ comment.authorName }}</span>
+                        <span v-else class="qv-author-anonymous">Anonymous</span>
+                    </div>
+                    <div class="qv-timestamp">
+                        {{ comment.timestamp | formatTimestamp() }}
+                    </div>
+                </div>
+                <div class="qv-text" v-html="comment.textHtml">
+               </div>
+            </li>
+        </ul>
     `
 })
 
@@ -106,7 +121,7 @@ Vue.component('qv-comment', {
         }
     },
     template: `
-        <div class="qv-comment">
+        <div>
             <div class="qv-metadata">
                 <div class="qv-author-avatar">
                     <img :src="comment.authorGravatar">
@@ -138,13 +153,17 @@ Vue.component('qv-comment-editor', {
             this.showingPreview = false
         },
         showPreview() {
-            this.$emit('get-preview', this.markdown)
-            this.showingPreview = true
+            if (this.markdown) {
+                this.previewStyle.width = this.$refs.textarea.offsetWidth - 10 + "px"
+                this.previewStyle.height = (this.$refs.textarea.offsetHeight - 10) + "px"
+                this.$emit('get-preview', this.markdown)
+                this.showingPreview = true
+            }
         },
         submitForm() {
             if (this.markdown) {
                 let comment = {
-                    path: "/",
+                    path: window.location.pathname,
                     authorName: this.name,
                     authorEmail: this.email,
                     text: this.markdown,
@@ -156,7 +175,7 @@ Vue.component('qv-comment-editor', {
                 this.markdown = null
                 this.error = null
             } else {
-                this.error = "Please enter some text before posting the comment."
+                this.error = "Please write something before posting the comment."
             }
         }
     },
@@ -166,34 +185,33 @@ Vue.component('qv-comment-editor', {
             email: null,
             markdown: null,
             showingPreview: false,
+            previewStyle: { width: "100%", height: "100px" },
             error: null
         }
     },
     template: `
         <div class="qv-comment-editor">
+            <h2 class="qv-editor-heading">Leave your comment</h2>
+            <p class="qv-editor-help">Your email address is used only for displaying your Gravatar. It won't be displayed in 
+            the comment and other people won't see it. Comments can be styled with <a href="http://daringfireball.net/projects/markdown/basics">markdown</a>.
+            Fenced code blocks are supported.</p>
             <form class="qv-comment-editor-form" @submit.prevent="submitForm">
-                <div class="qv-input-field qv-author-name-field">
-                    <input id="name" v-model="name" placeholder="Your name (optional)"> 
-                </div>
-                <div class="qv-input-field qv-author-email-field">
-                    <input id="name" v-model="email" placeholder="Your email address (optional)"> 
-                </div>
+                <input class="qv-input-field qv-author-name-field" id="name" v-model="name" placeholder="Your name (optional)"> 
+                <input class="qv-input-field qv-author-email-field" id="name" v-model="email" placeholder="Your email address (optional)"> 
                 <div class="qv-text-editor">
                     <span class="qv-tab" :class="{ 'qv-active-tab': !showingPreview }" @click="showTextArea()">Write</span>
                     <span class="qv-tab" :class="{ 'qv-active-tab': showingPreview }" @click="showPreview()">Preview</span>
                     <div class="qv-tab-content qv-textarea-field" v-show="!showingPreview">
-                        <textarea id="text" v-model="markdown" cols="80" rows="14"></textarea> 
+                        <textarea id="text" v-model="markdown" cols="80" rows="14" ref="textarea" style="width: 100%"></textarea> 
                     </div>
                     <div class="qv-tab-content" v-show="showingPreview">
-                        <div class="qv-text-preview" v-html="preview"></div>
+                        <div class="qv-text-preview" v-html="preview" v-bind:style="previewStyle"></div>
                     </div>
                 </div>
                 <div class="qv-submit-error" v-if="error">
                     {{ error }}
                 </div>
-                <div class="qv-submit">
-                    <input type="submit" value="Post comment">
-                </div>
+                <input class="qv-submit" type="submit" value="Post comment">
             </form>
         </div>
     `
